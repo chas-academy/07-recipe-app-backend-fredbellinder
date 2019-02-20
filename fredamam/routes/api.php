7c6624a5328f485a40da6e0a6bee23def7256e3d
Fred Bellinder <fred.bellinder@chasacademy.se>
2019-02-20 16:44:24 +0100

    Add Laravel/Passport.  Tokens can be created through signup and login.

--- fredamam/routes/api.php
+++ fredamam/routes/api.php
@@ -12,23 +12,42 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(
+    [
+    'prefix' => 'auth'
+    ],
+    function () {
+        Route::post('login', 'AuthController@login');
+        Route::post('signup', 'AuthController@signup');
+    
+        Route::group(
+            [
+            'middleware' => 'auth:api'
+            ],
+            function () {
+                Route::get('logout', 'AuthController@logout');
+                Route::get('user', 'AuthController@user');
+                Route::get('recipes', 'RecipeController@index');
+            }
+        );
+    }
+);
+// Route::middleware(
+//     'auth:api'
+// )
+//     ->get(
+//         '/user',
+//         function (Request $request) {
+//             return $request->user();
+//         }
+//     );
 
-Route::middleware(
-    'auth:api'
-)
-    ->get(
-        '/user',
-        function (Request $request) {
-            return $request->user();
-        }
-    );
+// Route::get('recipes', 'RecipeController@index');
 
-Route::get('recipes', 'RecipeController@index');
+// Route::get('recipe/{id}', 'RecipeController@index');
 
-Route::get('recipe/{id}', 'RecipeController@index');
+// Route::post('recipe', 'RecipeController@store');
 
-Route::post('recipe', 'RecipeController@store');
+// Route::put('recipes', 'RecipeController@store');
 
-Route::put('recipes', 'RecipeController@store');
-
-Route::delete('recipes', 'RecipeController@destroy');
+// Route::delete('recipes', 'RecipeController@destroy');
